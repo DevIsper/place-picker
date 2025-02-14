a103c184931ef8e0d4660eb7735e318a559decae
Gabriel Isper <2411600431@fema.edu.br>
2025-02-14 14:21:16 -0300

    chore: delay on sending images to user-images

--- backend/app.js
+++ backend/app.js
@@ -30,6 +30,7 @@ app.get("/places", async (req, res) => {
 });
 
 app.get("/user-places", async (req, res) => {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   const fileContent = await fs.readFile("./data/user-places.json");
 
   const places = JSON.parse(fileContent);
